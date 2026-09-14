5c3a5fa2800ce9e965a97358e2205adab95ca0b1
Viktor Johansson <discovicke@gmail.com>
2026-09-14 13:02:35 +0200

    Tickets

--- Program.cs
+++ Program.cs
@@ -1,6 +1,19 @@
+using AI_supportsystem;
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
+var tickets = new List<Ticket>
+{
+    new Ticket { Id = 1, Title = "Ticket 1", Description = "Description for Ticket 1" },
+    new Ticket { Id = 2, Title = "Ticket 2", Description = "Description for Ticket 2" },
+    new Ticket { Id = 3, Title = "Ticket 3", Description = "Description for Ticket 3" }
+};
+
 app.MapGet("/", () => "Hello World!");
+app.MapGet("/api/tickets", () =>
+{
+    return tickets;
+});
 
 app.Run();
---
+++ Ticket.cs
@@ -0,0 +1,9 @@
+namespace AI_supportsystem;
+
+public class Ticket
+{
+    public long Id { get; set; }
+    public string Title { get; set; }
+    public string Description { get; set; }
+    public string Status { get; set; }
+}
