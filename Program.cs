e097d5e7c66cc92f77dc01e490f5669e02365e36
Viktor Johansson <discovicke@gmail.com>
2026-09-14 13:04:59 +0200

    tickets

--- Program.cs
+++ Program.cs
@@ -5,15 +5,15 @@ var app = builder.Build();
 
 var tickets = new List<Ticket>
 {
-    new Ticket { Id = 1, Title = "Ticket 1", Description = "Description for Ticket 1" },
-    new Ticket { Id = 2, Title = "Ticket 2", Description = "Description for Ticket 2" },
-    new Ticket { Id = 3, Title = "Ticket 3", Description = "Description for Ticket 3" }
+    new Ticket { Id = 1, Title = "Aj", Description = "Gör ont när jag biter i folie", Status = "Open" },
+    new Ticket { Id = 2, Title = "Hur kan ni kalla detta för en leksak?", Description = "Yxan orsakar väggskador när min son slår med den mot fasaden", Status = "In Progress" },
+    new Ticket { Id = 3, Title = "Betala tillbaka!!!!!", Description = "Dubbel betalning för kyckling och keso på Ica Togo", Status = "Closed" }
 };
 
 app.MapGet("/", () => "Hello World!");
-app.MapGet("/api/tickets", () =>
-{
-    return tickets;
-});
+app.MapGet("/api/tickets", () => tickets);
+
+app.MapGet("/api/tickets/{id}", (int id) => tickets.FirstOrDefault(t => t.Id == id));
+app.MapPost("/api/tickets", (Ticket ticket) => tickets.Add(ticket));
 
 app.Run();
