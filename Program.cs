d3956df9421bd612fee0b099570e871cefd4fcdf
discotomten <linkaan_lonn@hotmail.com>
2026-09-14 13:25:39 +0200

    fix fix fix

--- Program.cs
+++ Program.cs
@@ -1,13 +1,15 @@
 using AI_supportsystem;
 using Azure;
 using Azure.AI.TextAnalytics;
+using DotNetEnv;
 
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
+Env.Load();
 // Eller hur det nu hämtas
-string endpoint = Environment.GetEnvironmentVariable("LANGUAGE_ENDPOINT");
-string key = Environment.GetEnvironmentVariable("LANGUAGE_KEY");
+string? endpoint = Environment.GetEnvironmentVariable("LANGUAGE_ENDPOINT");
+string? key = Environment.GetEnvironmentVariable("LANGUAGE_KEY");
 
 // Den viktiga biten
 var client = new TextAnalyticsClient(
@@ -19,34 +21,39 @@ var tickets = new List<Ticket>
 {
     new Ticket
     {
-        Id = 1, 
-        Title = "Aj", 
-        Description = "Gör ont när jag biter i folie", 
+        Id = 1,
+        Title = "Aj",
+        Description = "Gör ont när jag biter i folie",
         Status = Status.Open,
         Priority = Priority.High
     },
     new Ticket
     {
-        Id = 2, 
+        Id = 2,
         Title = "Hur kan ni kalla detta för en leksak?",
-        Description = "Yxan orsakar väggskador när min son slår med den mot fasaden", 
+        Description = "Yxan orsakar väggskador när min son slår med den mot fasaden",
         Status = Status.InProgress,
         Priority = Priority.Medium
     },
     new Ticket
     {
-        Id = 3, 
-        Title = "Betala tillbaka!!!!!", 
+        Id = 3,
+        Title = "Betala tillbaka!!!!!",
         Description = "Dubbel betalning för kyckling och keso på Ica Togo",
         Status = Status.Closed,
         Priority = Priority.Low
     }
 };
 
+app.UseStaticFiles();
+app.UseDefaultFiles();
+
 app.MapGet("/", () => "Hello World!");
 app.MapGet("/api/tickets", () => tickets);
 
 app.MapGet("/api/tickets/{id}", (int id) => tickets.FirstOrDefault(t => t.Id == id));
 app.MapPost("/api/tickets", (Ticket ticket) => tickets.Add(ticket));
+
+
 
 app.Run();
